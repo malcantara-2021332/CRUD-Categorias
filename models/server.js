90cb8d5e665091cb5790e5bb21ecbf2293e4c9bf
Marcos Alcantara <malcantara-2021332@kinal.edu.gt>
2023-02-08 10:15:57 -0600

    Primer commit V1

---
+++ models/server.js
@@ -0,0 +1,54 @@
+//Configuracion del server
+//Importaciones basicas
+const express = require('express');
+const cors = require('cors');
+const { dbConection } = require('../database/config');
+
+
+class Server {
+    constructor() {
+        //Variables de configuracion
+        this.app = express();
+        this.port = process.env.PORT;
+        this.categoriaPath = '/api/categoria';
+
+        //Conectar
+        this.conectarDB();
+
+        //Middlewares
+        this.middlewares();
+
+        //Rutas de mi app
+        this.routes();        
+    }
+
+    //Metodo de conection a Mongo
+    async conectarDB(){
+        await dbConection();
+    }
+
+    middlewares(){
+        //Cors
+        this.app.use(cors());
+
+        //Lectura y parseo del body
+        this.app.use(express.json());
+
+        //Directorio publico del proyecto
+        this.app.use(express.static('public'));
+    }
+
+    routes(){
+        //Referencia al archivo con las rutas
+        this.app.use(this.categoriaPath, require('../routes/categoria'));
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en pueto ${this.port}`)
+        })
+    }
+
+}
+
+module.exports = Server;
